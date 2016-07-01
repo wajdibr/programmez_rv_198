270f08f4558d6c0ad99da648a35cf75e650cd54e
wajdibenrabah <benrabah.wajdi@gmail.com>
2016-07-02 01:09:29 +0200

    [ADD] initial commit with the whole project

---
+++ Assets/scripts/CardboardCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class CardboardCollider : MonoBehaviour {
+
+	private CardboardHead head;
+	private Vector3 startingPosition;
+
+	void Start() {
+		head = Camera.main.GetComponent<StereoController>().Head;
+		startingPosition = transform.localPosition;
+	}
+
+	void Update() {
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		GetComponent<Renderer>().material.color = isLookedAt ? Color.green : Color.red;
+	}
+
+}
